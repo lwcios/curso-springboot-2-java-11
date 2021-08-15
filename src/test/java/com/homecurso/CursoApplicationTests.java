0ac5f984023c7792f18b913d32b42eebd59d796e
lwcios <lwciomawro@hotmail.com>
2021-08-15 14:12:10 -0300

    Project created

---
+++ src/test/java/com/homecurso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homecurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
