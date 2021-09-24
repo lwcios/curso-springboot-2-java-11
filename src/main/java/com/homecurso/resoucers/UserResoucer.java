5b95ec3e7d79333d7077ed88589f5cdec1516999
lwcios <lwciomawro@hotmail.com>
2021-09-24 17:01:38 -0300

    User insert

--- src/main/java/com/homecurso/resoucers/UserResoucer.java
+++ src/main/java/com/homecurso/resoucers/UserResoucer.java
@@ -1,13 +1,17 @@
 package com.homecurso.resoucers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.homecurso.entities.User;
 import com.homecurso.services.UserService;
@@ -29,12 +33,21 @@ public class UserResoucer {
 
 		return ResponseEntity.ok().body(list);
 	}
-	
-	@GetMapping(value ="/{id}")
-	public ResponseEntity<User> findById(@PathVariable Long id){
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User user = service.findById(id);
 		return ResponseEntity.ok().body(user);
-		
+
+	}
+
+	@PostMapping
+	public ResponseEntity<User> insertUser(@RequestBody User obj) {
+
+		obj = service.insertUser(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+
 	}
 
 }
--- src/main/java/com/homecurso/services/UserService.java
+++ src/main/java/com/homecurso/services/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.homecurso.entities.User;
@@ -14,16 +15,22 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
-	Optional<User> obj= repository.findById(id);
-	
-	return obj.get();
-		
+		Optional<User> obj = repository.findById(id);
+
+		return obj.get();
+
 	}
-	
+
+	public User insertUser(User obj) {
+
+		return repository.save(obj);
+
+	}
+
 }
