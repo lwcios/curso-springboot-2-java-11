edc5dee5d86916e4e581b5a68a80583f0f2336b8
lwcios <lwciomawro@hotmail.com>
2021-09-24 17:46:02 -0300

    User delete

--- src/main/java/com/homecurso/resoucers/UserResoucer.java
+++ src/main/java/com/homecurso/resoucers/UserResoucer.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,5 +50,14 @@ public class UserResoucer {
 		return ResponseEntity.created(uri).body(obj);
 
 	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> deletUser(@PathVariable Long id) {
+	  service.deletUser(id);
+	  return ResponseEntity.noContent().build();
+		
+		
+	}
+	
 
 }
